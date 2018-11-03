fb83efac6f7aab4fdbc3b308bdb98fb74a946b29
Harper Bellows <harper.bellows@gmail.com>
2018-11-03 13:39:15 -0600

    add google service call and response parsing methods

---
+++ app/services/google_service.rb
@@ -0,0 +1,28 @@
+class GoogleService
+  def initialize(location)
+    @location = location
+  end
+
+  def location_data
+    get_json("/maps/api/geocode/json?address=#{@location}")
+  end
+
+  def coordinates
+    location_data
+  end
+  
+
+  private
+  
+  def conn
+    Faraday.new(url: 'https://maps.googleapis.com') do |faraday|
+      faraday.headers['Accept'] = 'application/json'
+      faraday.params['key'] = ENV['google_api_key']
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+  
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
+end
