6e9154f38aff7483f248df2cefe84b5ce0f8ae53
Harper Bellows <harper.bellows@gmail.com>
2018-11-03 13:37:30 -0600

    add first functionality for google service call

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::ForecastController < ApiController
   def show
-    @coordinates = GoogleService.new(params[:location])
     # thing = ExternalApiService.new(params)
+    @location = GoogleService.new(params[:location])
     # thing_to_render_in_json = thing.method_name
+    coordinates = @location.coordinates
     # render json:  thing_to_render_in_json
+    render json: coordinates
   end
 end
