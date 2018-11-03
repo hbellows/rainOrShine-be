3ca434e656db7918e94157922c54eafa219ca64b
Harper Bellows <35637783+hbellows@users.noreply.github.com>
2018-11-03 13:44:28 -0600

    Update google_service_spec.rb

--- spec/services/google_service_spec.rb
+++ spec/services/google_service_spec.rb
@@ -1,15 +1,15 @@
 describe GoogleService do
-  it "exists" do
-    service = GoogleService.new("denver,co")
+  it 'exists' do
+    service = GoogleService.new('denver,co')
 
     expect(service).to be_a(GoogleService)
   end
 
-  context "instance methods" do
-    context "#location_data" do
-      it "returns a hash" do 
-        VCR.use_cassette("location_data") do
-          service = GoogleService.new("denver,co")
+  context 'instance methods' do
+    context '#location_data' do
+      it 'returns a hash' do 
+        VCR.use_cassette('location_data') do
+          service = GoogleService.new('denver,co')
           
           expect(service.location_data).to be_a(Hash)
           expect(service.location_data).to have_key(:results)
