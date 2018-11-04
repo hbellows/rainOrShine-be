3524f8010e2b317e318ad3b8b942562ae5e98027
Harper Bellows <35637783+hbellows@users.noreply.github.com>
2018-11-03 20:03:56 -0600

    Update forecast.rb

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -4,19 +4,19 @@ class Forecast
   end
 
   private
-  
+
   def geocoder_service
     @service ||= GoogleGeocoderService.new
   end
-  
+
   def get_location
     geocoder_service.location_data(@location)
   end
-  
+
   def coordinates
     get_location[:results][0][:geometry][:location]
   end
-  
+
   def latitude
     coordinates[:lat]
   end
