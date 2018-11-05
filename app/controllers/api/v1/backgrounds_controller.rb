4cfd354761671e02f9c16231c4389cff0c779bc1
Harper Bellows <harper.bellows@gmail.com>
2018-11-05 09:49:59 -0700

    move forecast.new to a private method, add before_action callback

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::BackgroundsController < ApiController
+  before_action :backgrounds
 
   def index
-    backgrounds = BackgroundImage.new(params[:location])
     render json: BackgroundImageSerializer.new(backgrounds).serialized_json
+  end
+
+  private
+
+  def backgrounds
+    @backgrounds ||= BackgroundImage.new(params[:location])
   end
 end
