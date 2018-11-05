e65c1314943c985076eccb793b17abab7c28979c
Harper Bellows <35637783+hbellows@users.noreply.github.com>
2018-11-04 17:41:34 -0700

    Update flickr_image_service.rb

--- app/services/flickr_image_service.rb
+++ app/services/flickr_image_service.rb
@@ -1,23 +1,19 @@
 class FlickrImageService
 
   def image_data(latitude, longitude)
-    get_json("/services/rest/?lat=#{latitude}&lon=#{longitude}")
+    get_json("/services/rest/?method=flickr.photos.search&lat=#{latitude}&lon=#{longitude}&per_page=10&page=1")
   end
 
   private
 
   def conn
     Faraday.new(url: 'https://api.flickr.com') do |faraday|
-      faraday.params['method'] = 'flickr.photos.search'
       faraday.params['api_key'] = ENV['flickr_api_key']
       faraday.params['format'] = 'json'
       faraday.params['nojsoncallback'] = '1'
       faraday.params['tags'] = 'building'
       faraday.params['privacy_filter'] = 'public'
       faraday.params['content_type'] = '1'
-      faraday.params['extras'] = 'url_o'
-      faraday.params['per_page'] = '10'
-      faraday.params['page'] = '1'
       faraday.adapter Faraday.default_adapter
     end
   end
