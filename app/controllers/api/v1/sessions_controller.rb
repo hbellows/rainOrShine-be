bb92ba9b7dda124f2f3ec6aa1d730b09cab56192
Harper Bellows <harper.bellows@gmail.com>
2019-01-03 08:11:45 -0700

    wip -> factory bot issues for user story 3

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,25 @@
 class Api::V1::SessionsController < ApiController
-
-
+  before_action :find_user, :validate_user
 
   def create
-    
+    render json: UserSerializer.new(find_user), status: 200
   end
 
-  private 
+  private
 
+  def session_params
+    params.permit(:email, :password)
+  end
+
+  def find_user
+    @find_user ||= User.find_by_email(session_params[:email])
+  end
+
+  def valid_password?
+    find_user.authenticate(session_params[:password])
+  end
+
+  def validate_user
+    render json: nil, status: 403 unless find_user && valid_password?
+  end
 end
