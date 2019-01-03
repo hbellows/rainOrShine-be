72b70fbd00462d32b59f7353f0a130a3b7b13044
Harper Bellows <harper.bellows@gmail.com>
2019-01-03 09:37:10 -0700

    more codecov config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'support/factory_bot'
+# require 'support/factory_bot'
 require 'webmock/rspec'
 require 'vcr'
 require 'simplecov'
@@ -31,6 +31,13 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+  config.include FactoryBot::Syntax::Methods
+
+  config.fuubar_progress_bar_options = { 
+    :format => "%a %b\u{15E7}%i %p%% %t",
+    :progress_mark  => ' ',
+    :remainder_mark => "\u{FF65}"
+  }
 
   config.filter_rails_from_backtrace!
 end
