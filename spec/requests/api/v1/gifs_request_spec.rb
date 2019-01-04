c08a65c3686b751540a2c29e046ab5c1814f8bec
Harper Bellows <harper.bellows@gmail.com>
2019-01-04 09:19:40 -0700

    add structure of gifs request spec

---
+++ spec/requests/api/v1/gifs_request_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe 'Gifs Endpoint' do
+  context 'GET request' do
+    it 'returns json with eight day forecast time, summary, and giphy url' do
+      
+    end
+  end
+end
