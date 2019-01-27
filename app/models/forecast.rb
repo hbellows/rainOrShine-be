eb917952d1a530bcf2f21aaf21e36aae7a7e4267
Harper Bellows <harper.bellows@gmail.com>
2019-01-26 21:50:50 -0700

    Add short form time for easier parsing on front end

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,8 +1,9 @@
 class Forecast
-  attr_reader :time, :summary, :icon, :temp, :feels_like, :humidity, :uv_index
+  attr_reader :time_long, :time_short, :summary, :icon, :temp, :feels_like, :humidity, :uv_index
 
   def initialize(data)
-    @time = Time.at(data[:time]).strftime('%I:%M %p')
+    @time_long = Time.at(data[:time]).strftime('%I:%M %p')
+    @time_short = Time.at(data[:time]).strftime('%I %p')
     @summary = data[:summary]
     @icon = data[:icon]
     @temp = data[:temperature].round
